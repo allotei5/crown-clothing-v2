78c73725592c68c528655c60e403c3776ce02336
allotei <allotei5@gmail.com>
2022-07-09 13:23:11 +0000

    created authentication component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,7 +4,8 @@ import {
     signInWithRedirect, 
     signInWithPopup, 
     GoogleAuthProvider,
-    createUserWithEmailAndPassword
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
 } from 'firebase/auth'
 
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
@@ -58,4 +59,13 @@ export const createUserDocumentFromAuth = async (userAuth, addionalInformation =
     return userDocRef;
 }
 
-export const createAuthUserWithEmailAndPassword = async (email, password) => createUserWithEmailAndPassword(auth, email, password)
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return ;
+    return await createUserWithEmailAndPassword(auth, email, password)
+}
+
+export const signInAuthWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password)
+}

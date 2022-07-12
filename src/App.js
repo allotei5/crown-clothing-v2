130104273328880e3b9a46ce069f367d428a90e2
allotei <allotei5@gmail.com>
2022-07-12 21:36:19 +0000

    created checkout page along with checkout items

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./routes/home/home.component";
 import Shop from "./routes/shop/shop.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./components/authentication/authentication.component"
+import Checkout from "./routes/checkout/checkout.component";
 
 
 
@@ -14,6 +15,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
